5a5d84806a3a45f6c023dd8d3fec173f80842751
nitro <nitro@dr-code.org>
2011-03-12 11:30:30 +0100

    - restructuring

---
+++ src/csharp/Convert.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FireBreath
+{
+    public static class Convert
+    {
+        public static Object ConvertToNet(this fbnetvariant value)
+        {
+            switch (value.get_type())
+            {
+                case "bool":
+                    return value.get_bool();
+                case "int":
+                    return value.get_int();
+                case "uint":
+                    return value.get_uint();
+                case "double":
+                    return value.get_double();
+                case "float":
+                    return value.get_float();
+                case "wstring":
+                    return value.get_wstring();
+                case "short":
+                    return value.get_short();
+                case "ushort":
+                    return value.get_ushort();
+                case "char":
+                    return value.get_char();
+                case "uchar":
+                    return value.get_uchar();
+                case "int64":
+                    return value.get_int64();
+                case "uint64":
+                    return value.get_uint64();
+                default:
+                    // throw bad cast exception here?
+                    return null;
+            }
+        }
+
+        public static fbnetvariant ConvertFromNet(this Object value)
+        {
+            fbnetvariant result = new fbnetvariant();
+            if (value is bool)
+                result.set((bool)value);
+            else if (value is int)
+                result.set((int)value);
+            else if (value is uint)
+                result.set((uint)value);
+            else if (value is double)
+                result.set((double)value);
+            else if (value is float)
+                result.set((float)value);
+            else if (value is string)
+                result.set((string)value);
+            else if (value is short)
+                result.set((short)value);
+            else if (value is ushort)
+                result.set((ushort)value);
+            else if (value is char)
+                result.set((char)value);
+            else if (value is byte)
+                result.set((byte)value);
+            else if (value is Int64)
+                result.set((Int64)value);
+            else if (value is UInt64)
+                result.set((UInt64)value);
+            else
+            {
+                // throw bad cast exception here?
+            }
+            return result;
+        }
+    }   
+}
---
+++ src/csharp/Main.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Windows.Forms;
+using FireBreath;
+
+namespace TestApp
+{
+    public class MyClass
+    {
+        public void ShowMessage(string msg)
+        {
+             MessageBox.Show(msg, "C# Message Box");
+        }
+    
+        public string data
+        {
+            get { return "123"; }
+        }
+    }
+
+    public class JSAPI : FBNetJSAPI
+    {
+        object globals;
+
+        public JSAPI(object Globals)
+        {
+            globals = Globals;
+        }
+
+        public override StringVector getMemberNames()
+        {
+            Type type = typeof(Globals);
+            if (type is Type)
+            {
+            }
+            type.GetMembers();
+            StringVector result = new StringVector();
+            result.Add("xxx");
+            return result;
+        }
+
+        public override uint getMemberCount()
+        {
+            return 1;
+        }
+
+        public override bool HasProperty(string propertyName)
+        {
+            return false;
+        }
+        public override bool HasProperty(int idx)
+        {
+            return true;
+        }
+
+        public override bool GetProperty(int idx, fbnetvariant value)
+        {
+            return true;
+        }
+        public override bool GetProperty(string propertyName, fbnetvariant value)
+        {
+            return true;
+        }
+
+        public override bool SetProperty(int idx, fbnetvariant value)
+        {
+            return true;
+        }
+        public override bool SetProperty(string propertyName, fbnetvariant value)
+        {
+            return true;
+        }
+
+        public override bool HasMethod(string methodName)
+        {
+            return false;
+        }
+
+        public override bool Invoke(string methodName, VariantVector args, fbnetvariant result)
+        {
+            return true;
+        }
+    }
+
+    class Globals
+    {
+        System.Type MyClass = typeof(MyClass);
+
+        public void run(JSAPI context)
+        {
+            //context.document.title = "Hello, this is the browser/js window";
+            string title = context.eval("document.title");
+            MessageBox.Show(title, "document.title is...");
+
+            context.eval(@"
+                var obj = new MyClass();
+                alert( obj.data );
+                obj.ShowMessage('Hello from JS');
+            ");
+        }
+    }
+
+    public static class MyExtension
+    {
+        public static JSAPI createJSAPI()
+        {
+            Globals globals = new Globals();
+            JSAPI api = new JSAPI(globals);
+            return api;
+        }
+    }
+
+}
