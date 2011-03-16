816e809b5c6403186a2ef6d3e11c9970f25a714a
Oliver Buchtala <oliver.buchtala@googlemail.com>
2011-03-17 00:21:53 +0100

    Remove obsolete debugging code from PyHelloWorld

--- projects/PyHelloWorld/PyHelloWorldAPI.cpp
+++ projects/PyHelloWorld/PyHelloWorldAPI.cpp
@@ -244,9 +244,6 @@ FB::variant PyHelloWorldAPI::hello_py_extension() {
     pJsApi = PyObject_CallObject(pCreateJSAPI, pArgs);
     Py_DECREF(pArgs);
 
-    // maybe error is due to refcounted destruction of pJsApi?
-    Py_INCREF(pJsApi);
-
     if (pJsApi == NULL) {
         result = FB::variant("'HelloWorldExtension.createJSAPI() failed'.");
         goto return_result;
@@ -281,7 +278,7 @@ FB::variant PyHelloWorldAPI::hello_py_extension() {
 
     JSAPI* jsapi = reinterpret_cast<JSAPI*>(ptr);
 
-    // attention: currently it is important not to take own
+    // leave python ownership (won't be decref'd until dll unload...)
     hello_py_ext_instance = FB::JSAPIPtr(jsapi, __JSAPI_no_delete);
 
     result = FB::variant(hello_py_ext_instance);
@@ -298,28 +295,3 @@ return_result:
     return result;
 
 }
-
-#ifndef SWIGEXPORT
-# if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
-#   if defined(STATIC_LINKED)
-#     define SWIGEXPORT
-#   else
-#     define SWIGEXPORT __declspec(dllexport)
-#   endif
-# else
-#   if defined(__GNUC__) && defined(GCC_HASCLASSVISIBILITY)
-#     define SWIGEXPORT __attribute__ ((visibility("default")))
-#   else
-#     define SWIGEXPORT
-#   endif
-# endif
-#endif
-
-extern
-SWIGEXPORT 
-#if PY_VERSION_HEX >= 0x03000000
-PyObject*
-#else
-void
-#endif
-init_FireBreath(void);
